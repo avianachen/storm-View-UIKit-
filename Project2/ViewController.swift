756e96eb484bf6d548d588dc68644830ed482d74
avianachen <17806790352@163.com>
2025-04-17 20:47:50 +0800

    在右侧栏添加显示分数

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -15,14 +15,23 @@ class ViewController: UIViewController {
     //保存我们游戏中使用的所有国家，同时我们将创建另一个属性来保存玩家的当前分数
     var countries = [String]()
     var correctAnswer = 0
-    var score = 0
+    var score = 0 {
+        didSet{
+            updateScoreLabel()
+        }
+    }
     var questionNum = 0
     var message: String?
+    private let scoreLabel = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "score", style: .plain, target: self, action: #selector(showScore))
+        scoreLabel.text = "分数：\(score)"
+        scoreLabel.textColor = .blue
+        scoreLabel.font = UIFont.systemFont(ofSize: 12)
+        scoreLabel.sizeToFit()//根据内容调整标签大小
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: scoreLabel)
         //我们可以通过将按钮的图层设置为 1 来解决这个问题，这样按钮周围就会画一条单点黑线.我们的边框在非视网膜设备上是 1 像素，在视网膜设备上是 2 像素，在视网膜高清设备上是 3 像素。由于自动将点乘以像素，这个边框在所有设备上的视觉厚度看起来都差不多。
         button1.layer.borderWidth = 1
         button2.layer.borderWidth = 1
@@ -43,7 +52,7 @@ class ViewController: UIViewController {
         button3.setImage(UIImage(named: countries[2]), for: .normal)
         //生成一个介于 0 到 2 之间的随机数（含 0 和 2）
         correctAnswer = Int.random(in: 0...2)
-        title = "玩家目前分数：\(score)，国家：\(countries[correctAnswer].uppercased())"
+        title = "国家：\(countries[correctAnswer].uppercased())"
     }
 
     //这个方法需要做三件事：
@@ -85,6 +94,11 @@ class ViewController: UIViewController {
         let alertController = UIAlertController(title: "你的分数", message: "你的分数是\(score)", preferredStyle: .alert)
         alertController.addAction(UIAlertAction(title: "确定", style: .default))
         present(alertController,animated: true)
+    }
+    
+    func updateScoreLabel(){
+        scoreLabel.text = "分数：\(score)"
+        scoreLabel.sizeToFit()//根据内容调整标签大小
     }
 }
 
